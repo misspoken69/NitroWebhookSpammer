edb9296eaa58cf89a8354ca761c45bd2ae88dd56
alex lol <74885892+misspoken69@users.noreply.github.com>
2021-02-22 19:55:06 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from discord_webhook import *
 print("{}".format(crayons.green('\nThis was made by Misspoken#1122! Not my fault if you get in trouble.')))
 
 def send():
-    webhook = DiscordWebhook(url=webhookurl, content="discord.gift/" + code)
+    webhook = DiscordWebhook(url=webhookurl, content="discord.gift/" + code) # First part of the generating.
     response = webhook.execute()
 
 # SETTINGS
-webhookurl = 'PUT YOUR WEBHOOK HERE'
+webhookurl = 'PUT YOUR WEBHOOK HERE' # Put your webhook here.
 
 changewh = input("Do you want to change the webhook URL, say Y if you haven't changed it in the file else say N. (Y/N) > ")
 if changewh == "y":
@@ -24,9 +24,9 @@ print("You have chose to send "+ amount +" codes.")
 
 for x in range(1, int(amount) + 1):
     code = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
-    send() # uses gay send def yes.
+    send() # Makes strings and sends them.
     print("Code Sent: " + code + "Code Number: %d" % (x))
-    time.sleep(1.65)
+    time.sleep(1.65) # Cooldown because of rate limited.
 
 print('\nAll the codes have been sent to your server!')
 time.sleep(5)
